d51ee454db84032dc913653c850a9e5ca05db3f8
krishnan05 <aayushkr@iitbhilai.ac.in>
2024-01-31 00:40:02 +0530

    Add items page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import NavBar from "./components/Navbar";
 import Body from "./pages/Body";
+import AddItem from "./pages/AddItem";
 
 function App() {
   const navigate = useNavigate();
@@ -22,6 +23,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Body />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/add_items" element={<AddItem />} />
       </Routes>
       </div>
   );
